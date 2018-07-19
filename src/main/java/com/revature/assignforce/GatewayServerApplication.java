582766d1d1dbd0c4b4702820d07f95220dec9ea2
Jay Feldman <jayfman@gmail.com>
2018-07-19 13:25:58 -0400

    reverting

--- src/main/java/com/revature/assignforce/GatewayServerApplication.java
+++ src/main/java/com/revature/assignforce/GatewayServerApplication.java
@@ -1,18 +1,10 @@
 package com.revature.assignforce;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -24,26 +16,4 @@ public class GatewayServerApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
-	
-	@Bean
-	public CorsFilter corsFilter() {
-		List<String> origins = new ArrayList<>();
-		origins.add("*");
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.setAllowedOrigins(origins);
-	    config.addAllowedHeader("*");
-	    config.addAllowedMethod("OPTIONS");
-	    config.addAllowedMethod("HEAD");
-	    config.addAllowedMethod("GET");
-	    config.addAllowedMethod("PUT");
-	    config.addAllowedMethod("POST");
-	    config.addAllowedMethod("DELETE");
-	    config.addAllowedMethod("PATCH");
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	}
-
-
 }
