1bff9b5e9f0c3f52eaa737001a53ae806f475908
tjansen17 <tnj@17cosmicln.net>
2018-07-19 22:25:50 -0400

    Hopefully this works

--- src/main/java/com/revature/assignforce/GatewayServerApplication.java
+++ src/main/java/com/revature/assignforce/GatewayServerApplication.java
@@ -2,6 +2,7 @@ package com.revature.assignforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class GatewayServerApplication {
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(GatewayServerApplication.class, args);
+		new SpringApplicationBuilder(GatewayServerApplication.class).run(args);
+
+//		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 }
