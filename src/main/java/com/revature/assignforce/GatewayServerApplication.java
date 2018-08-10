213fcc529ea8aef2f0a4ac3758226ef6c364e62e
August Duet <august.duet@revature.com>
2018-08-10 18:41:55 -0400

    change @EnableEurekaClient to @EnableDiscoveryClient [debug]

--- src/main/java/com/revature/assignforce/GatewayServerApplication.java
+++ src/main/java/com/revature/assignforce/GatewayServerApplication.java
@@ -2,13 +2,14 @@ package com.revature.assignforce;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 @CrossOrigin
 public class GatewayServerApplication {
 	
