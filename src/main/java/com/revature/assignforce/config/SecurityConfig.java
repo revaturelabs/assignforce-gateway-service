04399494f04a1eceea5d6a93eeb89214e432d57a
August Duet <august.duet@revature.com>
2018-08-14 10:11:18 -0400

    [debug] attempting to close access to downstream services

--- src/main/java/com/revature/assignforce/config/SecurityConfig.java
+++ src/main/java/com/revature/assignforce/config/SecurityConfig.java
@@ -21,8 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .cors().disable()
             .authorizeRequests()
-            .antMatchers(HttpMethod.OPTIONS, "/").permitAll()
-            .antMatchers("/").authenticated();
+            .antMatchers("/*").authenticated();
 
         JwtWebSecurityConfigurer
             .forRS256(audience, issuer)
