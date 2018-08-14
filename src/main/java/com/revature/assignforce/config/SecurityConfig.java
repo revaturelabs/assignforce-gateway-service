0e62a506d23b2131bba96308384f7364d0db4822
August Duet <august.duet@revature.com>
2018-08-14 11:02:27 -0400

    mastering this security beast [debug]

--- src/main/java/com/revature/assignforce/config/SecurityConfig.java
+++ src/main/java/com/revature/assignforce/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.revature.assignforce.config;
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,13 +23,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .cors().disable()
+            .cors()
+            .and()
+            .csrf().disable()
             .authorizeRequests()
-            .antMatchers(HttpMethod.OPTIONS, "/*/**").permitAll()
-            .antMatchers("/*/**").authenticated();
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .antMatchers("/**").authenticated();
 
         JwtWebSecurityConfigurer
             .forRS256(audience, issuer)
             .configure(http);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
