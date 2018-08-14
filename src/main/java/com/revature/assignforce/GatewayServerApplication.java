d35e3bc0e16a103ed4a315b34c4283063386c6a7
August Duet <august.duet@revature.com>
2018-08-14 13:12:57 -0400

    [debug]

--- src/main/java/com/revature/assignforce/GatewayServerApplication.java
+++ src/main/java/com/revature/assignforce/GatewayServerApplication.java
@@ -3,15 +3,13 @@ package com.revature.assignforce;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@EnableWebSecurity
+@CrossOrigin
 public class GatewayServerApplication {
 	
 	public static void main(String[] args) throws Exception {
