11d6a9d5c251b6d19d54f4ac8f53625e57e8458a
August Duet <august.duet@revature.com>
2018-08-14 10:26:00 -0400

    opening access to OPTIONS for preflight [debug]

--- src/main/java/com/revature/assignforce/config/SecurityConfig.java
+++ src/main/java/com/revature/assignforce/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .cors().disable()
             .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS, "/*/**").permitAll()
             .antMatchers("/*/**").authenticated();
 
         JwtWebSecurityConfigurer
