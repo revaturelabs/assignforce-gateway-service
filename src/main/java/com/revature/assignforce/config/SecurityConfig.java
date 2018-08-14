8805abf1988e74d8f36730baf8c2a96f612297aa
August Duet <august.duet@revature.com>
2018-08-13 22:29:48 -0400

    [debug]

--- src/main/java/com/revature/assignforce/config/SecurityConfig.java
+++ src/main/java/com/revature/assignforce/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.revature.assignforce.config;
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${auth0.audience}")
     private String audience;
