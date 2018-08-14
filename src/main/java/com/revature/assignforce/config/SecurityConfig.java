5f6d3be0c1d3d920ae6c7bc4e0a0e931667032df
August Duet <august.duet@revature.com>
2018-08-13 22:19:03 -0400

    [debug]

---
+++ src/main/java/com/revature/assignforce/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.revature.assignforce.config;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${auth0.issuer}")
+    private String issuer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/users").authenticated();
+
+        JwtWebSecurityConfigurer
+            .forRS256(audience, issuer)
+            .configure(http);
+    }
+}
