b7958ca68194355cad9b8cae906ab5eae5361bea
August Duet <august.duet@revature.com>
2018-08-14 13:21:29 -0400

    [debug]

--- src/main/java/com/revature/assignforce/GatewayServerApplication.java
+++ src/main/java/com/revature/assignforce/GatewayServerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 @CrossOrigin
+@EnableWebSecurity
 public class GatewayServerApplication {
 	
 	public static void main(String[] args) throws Exception {
